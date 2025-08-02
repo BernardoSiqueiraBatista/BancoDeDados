0728f9fd2d021490b0f1453b84ce4fdaa6b20dad
Theo Barza <tgsb@cin.ufpe.br>
2025-08-02 13:08:51 -0300

    novas consultas

--- consultas.py
+++ consultas.py
@@ -11,6 +11,9 @@ cursor.execute("""
     GROUP BY P.local_nacionalidade
     HAVING Quantidade > 0;
 """)
+print('Group by/Having:')
+print(cursor.fetchall())
+
 
 
 
@@ -21,6 +24,10 @@ cursor.execute("""
     FROM Participa P INNER JOIN Clube C 
         ON P.id_clube = C.id_clube;
 """)
+print('\nJunção interna:')
+print(cursor.fetchall())
+
+
 
 
 # Junção externa
@@ -31,5 +38,64 @@ cursor.execute("""
     ON P.cpf = J.cpf_jogador
     WHERE J.cpf_jogador IS NULL;
 """)
+print('\nJunção externa:')
+print(cursor.fetchall())
+
+
+
+
+# Subconsulta escalar
+# Exibir a quantidade de jogadores que nasceram a partir de 1995
+cursor.execute("""
+    SELECT (
+        SELECT COUNT(*)
+        FROM Jogador J
+        INNER JOIN PESSOA p on J.cpf_jogador = p.cpf
+        WHERE p.nascimento >= '1995-01-01'
+    ) AS quantidade_jogadores;
+""")
+print('\nSubconsulta escalar:')
+print(cursor.fetchone())
+
+
+
+
+# Subconsulta em linha
+# Obter nome e data de fundacao do clube mais antigo
+cursor.execute("""
+    SELECT nome, data_fundacao
+    FROM Clube
+    WHERE (nome, data_fundacao) = (
+        SELECT nome, data_fundacao
+        FROM Clube
+        ORDER BY data_fundacao
+        LIMIT 1
+    );
+""") 
+print('\nSubconsulta em linha:')
+print(cursor.fetchall())
+
+
+
+
+# Operacao conjunto
+# Mostrar o CPF das pessoas que sao técnicos OU árbitros
+cursor.execute("""
+    SELECT cpf_tecnico as cpf
+    FROM Tecnico
+    UNION
+    SELECT cpf_arbitro
+    FROM Arbitro;
+""")
+print('\nOperação conjunto:')
+print(cursor.fetchall())
+
+
+
+
+resultados = cursor.fetchall()
+
+for linha in resultados:
+    print(linha)
 
 connect.close()
